7484f592698340e3379dff8dd9d1400a51eb6c65
joshua-ftweb22 <you@example.com>
2022-11-02 23:05:12 +0530

    added prime problem

---
+++ prime_problem.js
@@ -0,0 +1,12 @@
+//**********prime problem***********
+let count=0;
+for(let i=1;i<=num;i++{
+if(num%i==0){
+count++;
+}
+}
+if(count==1){
+console.log("prime");
+}else{
+console.log("Not prime");
+}
